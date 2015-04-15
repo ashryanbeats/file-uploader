241e08e8906793748a3eb60e7ae66a6010ec6d78
Ash Ryan <ash@ashryanbeats.com>
2015-04-15 16:25:38 -0400

    Scss/css added. Gulpfile now watches scss and index.html for compile/livereload.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,26 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+
+gulp.task('sass', function() {
+    gulp.src('./app/assets/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./public/css'))
+        .pipe(livereload());
+});
+
+gulp.task('reload', function() {
+	gulp.src('./app/views/*.html')
+		.pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('./app/assets/*.scss', ['sass']);
+  gulp.watch('./app/views/*.html', ['reload']);
 });
